0f49e6b9c375808f1250c2c2ce84e1660e2566be
OpchetZ <varanyookasamsuk@gmail.com>
2023-12-25 12:24:08 +0700

    week6

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Covid19Controller;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StaffController;
 
 /*
 |--------------------------------------------------------------------------
@@ -148,3 +150,16 @@ Route::post("study-match", [ QuizController::class, "match" ])->name("study-matc
 // })->name('study-match');
 
 Route::get('/covid19', [ Covid19Controller::class,"index" ]);
+
+
+Route::get("/product", [ProductController::class, "index"])->name('product.index');
+Route::get("/product/create", [ProductController::class, "create"])->name('product.create');
+Route::post("/product", [ProductController::class, "store"])->name('product.store');
+Route::get('/product/{id}', [ProductController::class, "show"])->name('product.show');
+Route::get("/product/{id}/edit", [ProductController::class, "edit"])->name('product.edit');
+Route::patch("/product/{id}", [ProductController::class, "update"])->name('product.update');
+Route::delete("/product/{id}", [ProductController::class, "destroy"])->name('product.destroy');
+
+// Route::resource('/product', ProductController::class );
+
+Route::resource('/staff', StaffController::class );
