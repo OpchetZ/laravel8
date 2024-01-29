9e8aec94ddd7cd2090190164e31da1b1bfd06e17
OpchetZ <varanyookasamsuk@gmail.com>
2024-01-29 11:52:36 +0700

    week10

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,14 @@ use App\Http\Controllers\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Covid19Controller;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StaffController;
 use App\Http\Controllers\Surachet;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProfileController;  
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuotationController;
+use App\Http\Controllers\QuotationDetailController;
 use App\Http\Controllers\UserController;  
 use App\Http\Controllers\VehicleController;  
 
@@ -200,3 +203,21 @@ Route::resource('post', PostController::class);
 Route::resource('profile', ProfileController::class);
 Route::resource('user', UserController::class);
 Route::resource('vehicle', VehicleController::class);
+
+Route::resource('product', ProductController::class);
+
+// Route::resource('customer', 'CustomerController');
+// Route::resource('quotation', 'QuotationController');
+// Route::resource('quotation-detail', 'QuotationDetailController');
+
+
+//Laravel 8 Case Study : Quotation & Print PDF
+Route::middleware(['auth'])->group(function () {
+    Route::resource('customer', CustomerController::class);
+    Route::get('quotation/{id}/pdf', [QuotationController::class, 'pdf']);
+    Route::resource('quotation', QuotationController::class);
+    Route::resource('quotation-detail', QuotationDetailController::class);
+});
+
+
+
